f955661c5ac03261f9b77c07a5a3609a80a5ec52
Daniel Ang <dan.justin.ang@gmail.com>
2018-10-15 14:02:04 -0400

    [01_reinforcement_week8] completed

---
+++ 01_reinforce_week8.rb
@@ -0,0 +1,17 @@
+my_array = ["Totam", "ut", "odit","quis", "Maiores", "unde", "EX", "EST", "corporis"]
+
+def myFunction(array)
+  array.each do |word|
+    if word == word.downcase && word.length > 4
+      puts "long and lowercase"
+    elsif word == word.downcase
+      puts "lowercase"
+    elsif word.length > 4
+      puts "long"
+    else
+      puts word
+    end
+  end
+end
+
+myFunction(my_array)
